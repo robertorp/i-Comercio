cfd8382b2c996f4218d9bd3e878761ad487ae2a6
Roberto Alves <robertoalves@multimarcaseditoriais.com.br>
2025-10-04 11:31:07 -0300

    Add project files.

---
+++ i-Comercio/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace i_Comercio
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
